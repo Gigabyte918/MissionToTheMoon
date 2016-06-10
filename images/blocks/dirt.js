330e90c33f63e6643fe206c4cc40d4cb73e0fe5b
SharkFin - SharkFin Productions <sharkfinpro@hotmail.com>
2016-06-09 17:49:52 -0700

    Update dirt.js

--- images/blocks/dirt.js
+++ images/blocks/dirt.js
@@ -1,7 +1,16 @@
 function() { //leave the extra parameters empty
   //Draw The Image
-  
+  for(var y = 75; y < 100; y++) {
+    for(var x = 0; x < 25; x++) {
+      var noiseVal;
+      var noiseScale = 0.02;
+      noiseDetail(8,0.6);
+      noiseVal=noise((x+200) * noiseScale,(y+200) * noiseScale);
+      stroke(noiseVal * 160, noiseVal * 112, noiseVal * 0);
+      point(x, y);
+    }
+  }
 
   //Make the function display the image
-  return get(0, 0, 29, 29); //Returns a 27x27 square (25x25 square with room for a strokeweight of 2)
+  return get(0, 75, 25, 25);
 }
