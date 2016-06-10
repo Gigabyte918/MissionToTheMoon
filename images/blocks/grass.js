2100bd1a3c340275863bacfe20a80a0a95361417
SharkFin - SharkFin Productions <sharkfinpro@hotmail.com>
2016-06-09 17:48:19 -0700

    Update grass.js

--- images/blocks/grass.js
+++ images/blocks/grass.js
@@ -1,1 +1,26 @@
+function() { //leave the extra parameters empty
+  //Draw The Image
+  for(var y = 50; y < 55; y++) {
+            for(var x = 0; x < 25; x++) {
+                var noiseVal;
+                var noiseScale = 0.02;
+                noiseDetail(8,0.6);
+                noiseVal=noise((x+200) * noiseScale,(y+200) * noiseScale);
+                stroke(noiseVal * 0, noiseVal * 182, noiseVal * 0);
+                point(x, y);
+            }
+        }
+        for(var y = 55; y < 75; y++) {
+            for(var x = 0; x < 25; x++) {
+                var noiseVal;
+                var noiseScale = 0.02;
+                noiseDetail(8,0.6);
+                noiseVal=noise((x+200) * noiseScale,(y+200) * noiseScale);
+                stroke(noiseVal * 160, noiseVal * 112, noiseVal * 0);
+                point(x, y);
+            }
+        }
 
+  //Make the function display the image
+  return get(0, 50, 25, 25);
+}
